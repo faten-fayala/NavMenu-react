cf995e0d3630a7a0edf8e918a7aa6b1b00a4c6d5
faten-fayala <fatenfayala91@gmail.com>
2020-01-10 12:52:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Menu from './Menu';
 import './App.css';
 
+
+ let items= [ { 
+  text:'Home',
+  link:'#',
+  active:true 
+  } ,
+  { 
+  text:'Services',
+  link:'#',
+  active:false ,
+  submenu:['For entrepreneurs','For students','For hobbyists']
+  } ,
+  {
+   text:'Contact',
+  link:'#',
+  active:false
+  } 
+  ]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="App">
+     { 
+      items.map( el => <Menu data={el} /> )
+    }
+    </ul>
   );
 }
 
